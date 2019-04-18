574ad193ac14aa3f89ae50d1512138e376367ac3
Vivek Bansal <44487672+vivekbansal475@users.noreply.github.com>
2019-04-18 18:54:19 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function loadSkills(skills){
 		while(i<skills.length){
 
 			var row = '<div class="row">';
-			for(j=i;j<i+6&&j<skills.length;j++){
+			for(j=i;j<i+5&&j<skills.length;j++){
 				var skill = '<div class="col m2"><svg viewBox="0 0 128 128"><path d="'+skills[j].icon+'"></path></svg>'+skills[j].name+'</div>';
 				row+=skill;
 				
